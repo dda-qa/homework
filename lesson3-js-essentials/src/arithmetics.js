4be91d3d27787fb216ad94e18563e322e7e8e6ed
den <denrdav@gmail.com>
2025-02-03 16:48:48 +0100

    added homework for lesson 3

---
+++ lesson3-js-essentials/src/arithmetics.js
@@ -0,0 +1,14 @@
+let a = 1;
+let b = 0;
+let c = "hello"
+
+b--;
+a = b + c;
+console.log(a);
+
+b *= 2;
+console.log(b);
+
+let d = " world";
+
+console.log(c + d);
---
+++ lesson3-js-essentials/src/logical.js
@@ -0,0 +1,22 @@
+const a = 1;
+const b = 2;
+const c = true;
+const d = false;
+const e = '2';
+
+console.log(a < b);
+console.log(a > b);
+
+console.log(a == b);
+console.log(c == d);
+
+console.log(b == e);
+console.log(b === e);
+
+console.log(a && b);
+console.log (a || b);
+console.log(!a);
+
+console.log(c || d);
+console.log(c && d);
+console.log(!c);
